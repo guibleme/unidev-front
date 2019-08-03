298a60988b6a44847055e13f9d0f41b68a6a65fe
Guilherme Leme <guibleme@gmail.com>
2019-08-03 16:55:32 -0300

    commit inicial

---
+++ src/app/pages/importacao/importacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-importacao',
+  templateUrl: './importacao.component.html',
+  styleUrls: ['./importacao.component.scss']
+})
+export class ImportacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
